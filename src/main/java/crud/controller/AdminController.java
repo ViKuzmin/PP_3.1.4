8c2861a23c5672033794af7085ac99585c6510cf
VIKuzmin <11v.kuzmin@gmail.com>
2022-03-07 20:26:14 +0300

    add AdminController

--- src/main/java/crud/controller/AdminController.java
+++ src/main/java/crud/controller/AdminController.java
@@ -1,55 +1,55 @@
-//package crud.controller;
-//
-//import crud.models.User;
-//import crud.service.UserServiceImpl;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//
-//@Controller
-//@RequestMapping("/admin")
-//public class AdminController {
-//
-//    private final UserServiceImpl userServiceImpl;
-//
-//    @Autowired
-//    public AdminController(UserServiceImpl userServiceImpl) {
-//        this.userServiceImpl = userServiceImpl;
-//    }
-//
-//    @GetMapping
-//    public String getAllUsers(Model model) {
-//        model.addAttribute("users", userServiceImpl.findAll());
-//        return "user";
-//    }
-//
-//    @GetMapping("/new")
-//    public String addUserView(@ModelAttribute("user") User user) {
-//        return "new";
-//    }
-//
-//    @GetMapping("/{id}/edit")
-//    public String updateUserPage(Model model, @PathVariable long id) {
-//        model.addAttribute("user", userServiceImpl.findById(id));
-//        return "edit";
-//    }
-//
-//    @PostMapping
-//    public String addUser(@ModelAttribute("user") User user) {
-//        userServiceImpl.save(user);
-//        return "redirect:/user";
-//    }
-//
-//    @PutMapping("/{id}")
-//    public String updateUser(@ModelAttribute User user, @PathVariable long id) {
-//        userServiceImpl.save(user);
-//        return "redirect:/user";
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public String deleteUser(@PathVariable long id) {
-//        userServiceImpl.delete(id);
-//        return "redirect:/user";
-//    }
-//}
+package crud.controller;
+
+import crud.models.User;
+import crud.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserServiceImpl userServiceImpl;
+
+    @Autowired
+    public AdminController(UserServiceImpl userServiceImpl) {
+        this.userServiceImpl = userServiceImpl;
+    }
+
+    @GetMapping
+    public String getAllUsers(Model model) {
+        model.addAttribute("users", userServiceImpl.findAll());
+        return "user";
+    }
+
+    @GetMapping("/new")
+    public String addUserView(@ModelAttribute("user") User user) {
+        return "new";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String updateUserPage(Model model, @PathVariable long id) {
+        model.addAttribute("user", userServiceImpl.findById(id));
+        return "edit";
+    }
+
+    @PostMapping
+    public String addUser(@ModelAttribute("user") User user) {
+        userServiceImpl.save(user);
+        return "redirect:/admin";
+    }
+
+    @PutMapping("/{id}")
+    public String updateUser(@ModelAttribute User user, @PathVariable long id) {
+        userServiceImpl.save(user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable long id) {
+        userServiceImpl.delete(id);
+        return "redirect:/admin";
+    }
+}
